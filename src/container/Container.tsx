b246dcc3a6354874b84eca68882a57f8f6fce3a2
cteyr123 <cteyr123@gmail.com>
2022-05-22 23:26:14 -0400

    container design finishing

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -1,8 +1,54 @@
 const Container = () => {
   return (
     <div className="conatiner">
-      <div className="div-head"></div>
-      <div className="div-body"></div>
+      <div className="head-container">
+        <div className="head-tittle">
+          <h6>My balance</h6>
+          <p>$921.48</p>
+        </div>
+        <div className="circles">
+          <div className="circle-left"></div>
+          <div className="circle-right"></div>
+        </div>
+      </div>
+      <div className="body-container">
+        <div className="tittle-body">
+          <h3>Spanding - Last 7 days</h3>
+        </div>
+        <div className="grafic">
+          <span>
+            <div className="mon days" style={{ height: "100%" }}></div>mon
+          </span>
+          <span>
+            <div className="tue days"></div>tue
+          </span>
+          <span>
+            <div className="wed days"></div>wed
+          </span>
+          <span>
+            <div className="thu days"></div>thu
+          </span>
+          <span>
+            <div className="fri days"></div>fri
+          </span>
+          <span>
+            <div className="sat days"></div>sat
+          </span>
+          <span>
+            <div className="sun days"></div>sun
+          </span>
+        </div>
+        <div className="footer-container">
+          <div className="total-month">
+            <h6>Total this month</h6>
+            <p>$478.33</p>
+          </div>
+          <div className="last-month">
+            <p>+2.4%</p>
+            <p className="last-mont">from last month</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
